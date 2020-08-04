02976799027c13cf62561beaf2e6215007dc6cdc
RDasher64 <55093609+RDasher64@users.noreply.github.com>
2020-08-04 10:26:23 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write a number: ");
+            int Num = int.Parse(Console.ReadLine());
+            int Rem;
+            Stack Result = new Stack();
+            while (Num > 0)
+            {
+                Rem = Num % 2;
+                Result.Push(Rem);
+                Num = Num / 2;
+            }
+            foreach (var item in Result)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
